5edbed5a47399fa845401d62e0619a5087bc6b8f
Joao Esperancinha <jofisaes@gmail.com>
2022-01-23 22:22:50 +0100

    🌿 Coveralls headers for Git

--- omni-coveragereporter-python/coveralls_converter.py
+++ omni-coveragereporter-python/coveralls_converter.py
@@ -1,2 +1,31 @@
+import os
+
+import git
+
+repo = git.Repo(os.getcwd())
+master = repo.head.reference
+commit = master.commit
+commitHex = commit.hexsha
+
+
 def convert_coverage(data):
-    return None
+    coverallls_report = {}
+    coverallls_report['repoToken'] = os.getenv('COVERALLS_REPO_TOKEN')
+    coverallls_report['serviceName'] = 'local-ci'
+    head = {
+        'id': commitHex,
+        'authorName': commit.author.name,
+        'authorEmail': commit.author.email,
+        'committerName': commit.committer.name,
+        'committerEmail': commit.committer.email,
+        'message': commit.message
+    }
+    remotes = list(map(lambda remote: {'name': remote.name, 'url': remote.url}, repo.remotes))
+    print(remotes)
+    git_repo = {
+        'head': head,
+        'branch': repo.active_branch.name,
+        'remotes': remotes
+    }
+    coverallls_report['git'] = git_repo
+    return coverallls_report
