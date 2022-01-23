36835522e7e6fe7c1cb5f6206dba8089f0b975af
Joao Esperancinha <jofisaes@gmail.com>
2022-01-23 19:08:01 +0100

    🌿 Coveralls Baseline

--- omni-coveragereporter-python/omni_coveragereporter_python.py
+++ omni-coveragereporter-python/omni_coveragereporter_python.py
@@ -3,20 +3,17 @@ import json
 import codecov_converter
 import codecov_client
 
+import coveralls_converter
+
 f = open('coverage.json')
-
 data = json.load(f)
-
 f.close()
 
 codecov_report = codecov_converter.convert_coverage(data)
-
 print(json.dumps(codecov_report))
-
 codecov_report = codecov_converter.convert_coverage(data, codecov_report)
-
 print(json.dumps(codecov_report))
-
 report = codecov_client.send_report([json.dumps(codecov_report)])
-
 print(report)
+
+coveralls_report = coveralls_converter.convert_coverage(data)
