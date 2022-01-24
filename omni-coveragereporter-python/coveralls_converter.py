413d00ee05665404583e27f1d0194464f56efbe8
Joao Esperancinha <jofisaes@gmail.com>
2022-01-24 08:07:19 +0100

    🌿 MD5 hash

--- omni-coveragereporter-python/coveralls_converter.py
+++ omni-coveragereporter-python/coveralls_converter.py
@@ -1,3 +1,4 @@
+import hashlib
 import os
 
 import git
@@ -8,26 +9,42 @@ commit = master.commit
 commitHex = commit.hexsha
 
 
-def convert_coverage(data):
-    coverallls_report = {}
-    coverallls_report['repoToken'] = os.getenv('COVERALLS_REPO_TOKEN')
-    coverallls_report['serviceName'] = 'local-ci'
-    head = {
-        'id': commitHex,
-        'authorName': commit.author.name,
-        'authorEmail': commit.author.email,
-        'committerName': commit.committer.name,
-        'committerEmail': commit.committer.email,
-        'message': commit.message
-    }
-    remotes = list(map(lambda remote: {'name': remote.name, 'url': remote.url}, repo.remotes))
-    print(remotes)
-    git_repo = {
-        'head': head,
-        'branch': repo.active_branch.name,
-        'remotes': remotes
-    }
-    source_files = {}
-    coverallls_report['sourceFiles'] = source_files
-    coverallls_report['git'] = git_repo
-    return coverallls_report
+def convert_coverage(data, report = None):
+    if report:
+        coveralls_files = report['sourceFiles']
+        coveralls_report = report
+    else:
+        coveralls_files = {}
+        coveralls_report = {
+            'repoToken': os.getenv('COVERALLS_REPO_TOKEN'),
+            'serviceName': 'local-ci'}
+        head = {
+            'id': commitHex,
+            'authorName': commit.author.name,
+            'authorEmail': commit.author.email,
+            'committerName': commit.committer.name,
+            'committerEmail': commit.committer.email,
+            'message': commit.message
+        }
+        remotes = list(map(lambda remote: {'name': remote.name, 'url': remote.url}, repo.remotes))
+        print(remotes)
+        git_repo = {
+            'head': head,
+            'branch': repo.active_branch.name,
+            'remotes': remotes
+        }
+        coveralls_report['git'] = git_repo
+        coveralls_report['sourceFiles'] = []
+    source_files = coveralls_report['sourceFiles']
+    for fileName in data['files']:
+        file_object = data['files'][fileName]
+        text_file = open(fileName, "r")
+        file_content_bytes = text_file.read().encode('utf-8')
+        source_file = {
+            'name': fileName,
+            'sourceDigest': hashlib.md5(file_content_bytes).hexdigest(),
+            'coverage': [],
+        }
+        source_files.append(source_file)
+
+    return coveralls_report
