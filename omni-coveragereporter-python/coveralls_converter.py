b67643536cb16406dcea5ace75bbafa0ae62fb70
Joao Esperancinha <jofisaes@gmail.com>
2022-01-24 09:34:56 +0100

    🌿 Coverage calculator for Coveralls

--- omni-coveragereporter-python/coveralls_converter.py
+++ omni-coveragereporter-python/coveralls_converter.py
@@ -2,6 +2,7 @@ import hashlib
 import os
 
 import git
+
 import coveragepy_parser
 
 repo = git.Repo(os.getcwd())
@@ -10,7 +11,7 @@ commit = master.commit
 commitHex = commit.hexsha
 
 
-def convert_coverage(data, report = None):
+def convert_coverage(data, report=None):
     if report:
         coveralls_files = report['sourceFiles']
         coveralls_report = report
@@ -42,10 +43,16 @@ def convert_coverage(data, report = None):
         text_file = open(fileName, "r")
         file_content_bytes = text_file.read().encode('utf-8')
         total_lines = coveragepy_parser.total_lines(file_object)
+        line_coverage = [None] * total_lines
+        executed_lines = file_object['executed_lines']
+        for entry in executed_lines:
+            print(total_lines)
+            print(entry)
+            line_coverage[entry - 1] = 1 if entry in executed_lines else 0
         source_file = {
             'name': fileName,
             'sourceDigest': hashlib.md5(file_content_bytes).hexdigest(),
-            'coverage': [],
+            'coverage': line_coverage,
         }
         source_files.append(source_file)
 
