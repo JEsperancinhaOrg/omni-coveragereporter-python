2eb08e564c87b5d2899c37d714c55ca9a648a602
Joao Esperancinha <jofisaes@gmail.com>
2022-02-04 11:55:16 +0100

    🌿 Typo fix

--- omni-coveragereporter-python/src/omni_coveragereporter/report_detector.py
+++ omni-coveragereporter-python/src/omni_coveragereporter/report_detector.py
@@ -1,5 +1,6 @@
 def is_coverage_py(report_text):
-    if report_text.startswith("{") and report_text.endswith("}") and "\"meta\":" in report_text and "\"files\":" in report_text:
+    if report_text.startswith("{") and report_text.endswith(
+            "}") and "\"meta\":" in report_text and "\"files\":" in report_text:
         return True
     return False
 
